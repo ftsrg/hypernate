df8ea2e37016d5be1e25ff0bc9439255a3fe0b8b
bzp99 <bpeter@edu.bme.hu>
2024-05-15 20:27:38 +0200

    fix: create registry dynamically in context
    We cannot create a Registry instance at construction time of
    ContextWithRegistry, because Registry needs a stub instance.  However,
    the 'head' stub instance is unknown at the construction of
    ContextWithRegistry (normally, from a child class).
    
    At the same time, it makes sense to create the Registry on the fly.
    Context is an ephemeral object anyway and normally, one would only
    invoke getRegistry once and then reuse that.
    
    Signed-off-by: bzp99 <bpeter@edu.bme.hu>

--- lib/src/main/java/hu/bme/mit/ftsrg/hypernate/context/ContextWithRegistry.java
+++ lib/src/main/java/hu/bme/mit/ftsrg/hypernate/context/ContextWithRegistry.java
@@ -2,7 +2,6 @@
 package hu.bme.mit.ftsrg.hypernate.context;
 
 import hu.bme.mit.ftsrg.hypernate.Registry;
-import lombok.Getter;
 import org.hyperledger.fabric.contract.Context;
 import org.hyperledger.fabric.shim.ChaincodeStub;
 
@@ -11,13 +10,13 @@ import org.hyperledger.fabric.shim.ChaincodeStub;
  *
  * <p>The registry can be used to manage entities.
  */
-@Getter
 public class ContextWithRegistry extends Context {
-
-  private final Registry registry;
 
   public ContextWithRegistry(final ChaincodeStub stub) {
     super(stub);
-    registry = new Registry(stub);
+  }
+  
+  public Registry getRegistry() {
+    return new Registry(getStub());
   }
 }
