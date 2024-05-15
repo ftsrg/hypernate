12f4d87a5700bf78de0712da451e59136d3856fc
bp99 <bertalan.peter@bp99.eu>
2024-05-15 13:51:03 +0200

    refactor: remove OpenJML
    Hypernate itself at the moment does not require OpenJML.  There is no
    reason to include it in the build process.  Instead, projects building
    on top of Hypernate may use OpenJML if necessary.

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -1,21 +1,6 @@
 /* SPDX-License-Identifier: Apache-2.0 */
 
-/* Originally based on https://github.com/mingyang91/openjml-template */
-
-import com.diffplug.gradle.spotless.SpotlessExtension
-import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
-import hu.bme.mit.ftsrg.gradle.openjml.*
 import org.gradle.api.tasks.testing.logging.TestLogEvent
-
-val openJMLDir: Directory = layout.projectDirectory.dir(".openjml")
-val openJMLJavaHomeDir: Directory = openJMLDir.dir("jdk")
-val downloadDir: Provider<Directory> = layout.buildDirectory.dir("tmp/download")
-
-val jmlavac: RegularFile = openJMLJavaHomeDir.file("bin/jmlavac")
-val jmlava: RegularFile = openJMLJavaHomeDir.file("bin/jmlava")
-
-val withoutOpenJML: String? by project
-val noOpenJML: Boolean = withoutOpenJML != null && withoutOpenJML.toBoolean()
 
 plugins {
   `java-library`
@@ -47,57 +32,10 @@ dependencies {
   testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
   testImplementation("org.mockito:mockito-core:5.11.0")
   testImplementation("org.mockito:mockito-junit-jupiter:5.11.0")
-  testImplementation(files("$openJMLDir/jmlruntime.jar"))
 }
 
 tasks.withType<JavaCompile> {
   options.compilerArgs.addAll(listOf("-Xlint:unchecked", "-Xlint:deprecation"))
-}
-
-tasks.register("initOpenJML") {
-  val openJMLVersion: String by project
-
-  val zipFile: File = downloadDir.get().file("openjml.zip").asFile
-  downloadOpenJML(openJMLVersion, zipFile, logger)
-  extractOpenJML(zipFile, openJMLDir, logger)
-
-  // `jmlavac' is what we call `javac' that is actually
-  // OpenJML's javac; likewise, `jmlava' is a wrapper for `java' with
-  // OpenJML already in the classpath
-  generateJmlavac(jmlavac.asFile, openJMLJavaHomeDir, logger)
-  replaceJavac(openJMLJavaHomeDir, jmlavac.asFile, logger)
-  generateJmlava(jmlava.asFile, openJMLJavaHomeDir, logger)
-  replaceJava(openJMLJavaHomeDir, jmlava.asFile, logger)
-  logger.lifecycle("✅ OpenJML successfully initialized in $openJMLDir")
-}
-
-if (!noOpenJML) {
-  tasks.named<ShadowJar>("shadowJar") { dependsOn(tasks.named("initOpenJML")) }
-
-  tasks.test { java { jvmArgs = listOf("-Dorg.jmlspecs.openjml.rac=exception") } }
-
-  tasks.withType<JavaCompile>().configureEach {
-    dependsOn(tasks.named("initOpenJML"))
-    // Only when not compiling because of Spotless
-    if (!gradle.startParameter.taskNames.any { it.contains("spotlessApply") }) {
-      val mode =
-          when (System.getenv("JML_MODE")) {
-            "esc" -> "esc"
-            else -> "rac"
-          }
-      options.isFork = true
-      options.compilerArgs.addAll(
-          listOf(
-              "-jml",
-              "-$mode",
-              "-timeout",
-              "30",
-              "--nullable-by-default",
-              "--specs-path",
-              "specs/"))
-      options.forkOptions.javaHome = openJMLJavaHomeDir.asFile
-    }
-  }
 }
 
 tasks.test {
@@ -108,7 +46,7 @@ tasks.test {
   }
 }
 
-configure<SpotlessExtension> {
+spotless {
   java {
     importOrder()
     removeUnusedImports()
