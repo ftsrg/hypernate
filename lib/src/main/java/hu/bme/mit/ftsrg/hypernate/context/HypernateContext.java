65c08af89ee423911ce30ece28628d8913b9e38a
bp99 <bertalan.peter@bp99.eu>
2025-02-06 00:02:48 +0100

    feat: implement event raise functionality
    Also adds a HypernateContract contract base class that automatically
    raises the Transaction{Start,End} events in
    the {before,after}Transaction methods.

--- lib/src/main/java/hu/bme/mit/ftsrg/hypernate/context/HypernateContext.java
+++ lib/src/main/java/hu/bme/mit/ftsrg/hypernate/context/HypernateContext.java
@@ -3,6 +3,7 @@ package hu.bme.mit.ftsrg.hypernate.context;
 
 import hu.bme.mit.ftsrg.hypernate.Registry;
 import hu.bme.mit.ftsrg.hypernate.middleware.ChaincodeStubMiddleware;
+import hu.bme.mit.ftsrg.hypernate.middleware.event.HypernateEvent;
 import java.util.LinkedList;
 import java.util.List;
 import org.hyperledger.fabric.contract.Context;
@@ -15,7 +16,7 @@ import org.hyperledger.fabric.shim.ChaincodeStub;
  */
 public class HypernateContext extends Context {
 
-  private final List<ChaincodeStub> middlewares = new LinkedList<>();
+  private final List<ChaincodeStubMiddleware> middlewares = new LinkedList<>();
 
   public HypernateContext(final ChaincodeStub stub) {
     super(stub);
@@ -25,11 +26,15 @@ public class HypernateContext extends Context {
     return new Registry(getStub());
   }
 
-  public void installMiddleware(ChaincodeStub middleware) {
+  public void installMiddleware(ChaincodeStubMiddleware middleware) {
     middlewares.add(middleware);
   }
 
-  public void uninstallMiddleware(ChaincodeStub middleware) {
+  public void uninstallMiddleware(ChaincodeStubMiddleware middleware) {
     middlewares.remove(middleware);
+  }
+
+  public void raiseEvent(final HypernateEvent event) {
+    middlewares.forEach(mw -> mw.onEvent(event));
   }
 }
