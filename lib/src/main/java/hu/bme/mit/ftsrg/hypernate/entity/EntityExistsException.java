a839c78c5d6ee3b837b836ebde6a2109f3e85b09
bp99 <bertalan.peter@bp99.eu>
2025-02-18 22:24:10 +0100

    feat: add common HypernateException base class

--- lib/src/main/java/hu/bme/mit/ftsrg/hypernate/entity/EntityExistsException.java
+++ lib/src/main/java/hu/bme/mit/ftsrg/hypernate/entity/EntityExistsException.java
@@ -1,7 +1,10 @@
 /* SPDX-License-Identifier: Apache-2.0 */
 package hu.bme.mit.ftsrg.hypernate.entity;
 
+import lombok.experimental.StandardException;
+
 /** Exception thrown when the {@link Entity} to create already exists. */
+@StandardException
 public class EntityExistsException extends DataAccessException {
 
   public EntityExistsException(final String key) {
