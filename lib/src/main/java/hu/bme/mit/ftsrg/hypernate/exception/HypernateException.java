00d58d266d7874dd2edaa1701cf97054ff2044d7
bp99 <bertalan.peter@bp99.eu>
2025-02-18 22:24:10 +0100

    feat: add common HypernateException base class

---
+++ lib/src/main/java/hu/bme/mit/ftsrg/hypernate/exception/HypernateException.java
@@ -0,0 +1,7 @@
+package hu.bme.mit.ftsrg.hypernate.exception;
+
+import lombok.experimental.StandardException;
+
+/** Based class for all Hypernate-related exceptions. */
+@StandardException
+public abstract class HypernateException extends RuntimeException {}
