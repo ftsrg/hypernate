159a3855659c4e9e7147b5df57a9cd89dc3bbc34
bp99 <bertalan.peter@bp99.eu>
2025-02-28 14:24:18 +0100

    refactor: remove exception subpackage

--- lib/src/main/java/hu/bme/mit/ftsrg/hypernate/entity/DataAccessException.java
+++ lib/src/main/java/hu/bme/mit/ftsrg/hypernate/entity/DataAccessException.java
@@ -1,7 +1,7 @@
 /* SPDX-License-Identifier: Apache-2.0 */
 package hu.bme.mit.ftsrg.hypernate.entity;
 
-import hu.bme.mit.ftsrg.hypernate.exception.HypernateException;
+import hu.bme.mit.ftsrg.hypernate.HypernateException;
 import lombok.experimental.StandardException;
 
 /** Base class for {@link HypernateException}s related to data access problems. */
