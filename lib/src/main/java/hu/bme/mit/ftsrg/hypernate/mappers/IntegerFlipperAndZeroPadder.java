72f18d36015b77f71d1ffb23b19595081ba0588d
Attila Klenik <a.klenik@gmail.com>
2025-03-12 21:46:55 +0100

    fix: introduce attribute mapper base class #52
    Signed-off-by: Attila Klenik <a.klenik@gmail.com>

--- lib/src/main/java/hu/bme/mit/ftsrg/hypernate/mappers/IntegerFlipperAndZeroPadder.java
+++ lib/src/main/java/hu/bme/mit/ftsrg/hypernate/mappers/IntegerFlipperAndZeroPadder.java
@@ -1,14 +1,16 @@
 /* SPDX-License-Identifier: Apache-2.0 */
 package hu.bme.mit.ftsrg.hypernate.mappers;
 
-import java.util.function.Function;
-
-public class IntegerFlipperAndZeroPadder implements Function<Integer, String> {
+public class IntegerFlipperAndZeroPadder implements AttributeMapper {
   private static final String FORMAT_STRING =
       "%0" + String.valueOf(Integer.MAX_VALUE).length() + "d";
 
   @Override
-  public String apply(Integer integer) {
-    return String.format(IntegerFlipperAndZeroPadder.FORMAT_STRING, Integer.MAX_VALUE - integer);
+  public String apply(Object object) {
+    if (object instanceof Integer integer && integer >= 0) {
+      return String.format(IntegerFlipperAndZeroPadder.FORMAT_STRING, Integer.MAX_VALUE - integer);
+    }
+
+    throw new IllegalArgumentException("The IntegerFlipperAndZeroPadder attribute mapper only supports positive integer intputs");
   }
 }
--- lib/src/main/java/hu/bme/mit/ftsrg/hypernate/mappers/LongFlipperAndZeroPadder.java
+++ lib/src/main/java/hu/bme/mit/ftsrg/hypernate/mappers/LongFlipperAndZeroPadder.java
@@ -1,13 +1,15 @@
 /* SPDX-License-Identifier: Apache-2.0 */
 package hu.bme.mit.ftsrg.hypernate.mappers;
 
-import java.util.function.Function;
-
-public class LongFlipperAndZeroPadder implements Function<Long, String> {
+public class LongFlipperAndZeroPadder implements AttributeMapper {
   private static final String FORMAT_STRING = "%0" + String.valueOf(Long.MAX_VALUE).length() + "d";
 
   @Override
-  public String apply(Long number) {
-    return String.format(LongFlipperAndZeroPadder.FORMAT_STRING, Integer.MAX_VALUE - number);
+  public String apply(Object object) {
+    if (object instanceof Long longNumber && longNumber >= 0) {
+      return String.format(LongFlipperAndZeroPadder.FORMAT_STRING, Long.MAX_VALUE - longNumber);
+    }
+
+    throw new IllegalArgumentException("The LongFlipperAndZeroPadder attribute mapper only supports positive long number intputs");
   }
 }
